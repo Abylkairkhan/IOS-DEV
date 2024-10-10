ef6bd51f2b0415acd29c5eab4b3f661800b4ec39
Abylkairkhan <a_sarsetaev@kbtu.kz>
2024-10-10 22:35:33 +0500

    Assignment 4

--- Dicee/Dicee/ViewController.swift
+++ Dicee/Dicee/ViewController.swift
@@ -8,12 +8,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    let diceArray = [
+        UIImage(named: "dice-one"),
+        UIImage(named: "dice-two"),
+        UIImage(named: "dice-three"),
+        UIImage(named: "dice-four"),
+        UIImage(named: "dice-five"),
+        UIImage(named: "dice-six"),
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        if let diceImage1 = diceArray.randomElement() as? UIImage {
+                    diceImageView1.image = diceImage1
+                }
+                if let diceImage2 = diceArray.randomElement() as? UIImage {
+                    diceImageView2.image = diceImage2
+                }
+    }
 }
 
